2837084acae6ae611f76b563457c3beefe0c6aca
Neri <nerisanchz12@gmail.com>
2025-05-25 17:49:35 -0600

    Boton Actualizado Neri

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,8 +39,8 @@
                 <li>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="block py-2 px-4 rounded bg-red-600 hover:bg-red-800 text-white">
-                            <i class="fas fa-sign-out-alt mr-2"></i> Logout
+                         <button type="submit" class="w-full flex items-center gap-2 py-2 px-4 rounded bg-green-500 hover:bg-green-600 text-black font-semibold transition duration-200">
+                            <i class="fas fa-sign-out-alt mr-2"></i> Salir
                         </button>
                     </form>
                 </li>
